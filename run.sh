d91b40843b1e6051e92e09e6a8ab86cb7c2cb26c
Fabio Teixeira <fabio1079@gmail.com>
2021-07-15 15:43:45 -0300

    Configura docker para passar variaveis de ambiente para o php

--- run.sh
+++ run.sh
@@ -4,7 +4,11 @@ mkdir -p application/logs
 chown www-data:www-data application/logs
 
 # php service
-service php7.3-fpm start
+# service php7.3-fpm start
+
+# https://serverfault.com/questions/813368/configure-php-fpm-to-access-environment-variables-in-docker
+#/etc/init.d/php7.3-fpm start && nginx -g 'daemon off;'
+/etc/init.d/php7.3-fpm start
 
 # http server
 service nginx start 
