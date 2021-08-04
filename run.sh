f32d37e79c956e029c2176ac9b8c89f68756953b
David Carlos <ddavidcarlos1392@gmail.com>
2021-08-04 17:07:15 -0300

    Corrige script que sobe servidor php

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ mkdir -p application/logs
 chown www-data:www-data application/logs
 
 # https://serverfault.com/questions/813368/configure-php-fpm-to-access-environment-variables-in-docker
-sed -i "s#;clear_env = no#clear_env = no#" /etc/php/7.2/fpm/pool.d/www.conf
+sed -i "s#;clear_env = no#clear_env = no#" /etc/php/7.3/fpm/pool.d/www.conf
 
 # php service
 /etc/init.d/php7.3-fpm start
